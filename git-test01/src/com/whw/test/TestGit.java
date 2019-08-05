e8708396f8dc33a43ccdbe39ccbaa345b1891e0d
Wu <18888107019@163.com>
2019-08-05 16:39:15 +0800

    把本地工程添加到git

---
+++ git-test01/src/com/whw/test/TestGit.java
@@ -0,0 +1,12 @@
+package com.whw.test;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+        System.out.println("hello git1");
+
+    }
+}
